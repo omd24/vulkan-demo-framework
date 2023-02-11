b4025d15b1c8dbbdbd7f35135c0d2e1c35ee28d4
omd24 <omid.miresmaeili@gmail.com>
2023-02-11 19:34:34 +0100

    #ADD cglm

--- VulkanDemoFramework/Samples/01-glTFLoading/01-glTFLoading.cpp
+++ VulkanDemoFramework/Samples/01-glTFLoading/01-glTFLoading.cpp
@@ -5,6 +5,15 @@
 #include <Foundation/Time.hpp>
 
 #include <Application/Window.hpp>
+#include <Application/Input.hpp>
+#include <Application/Keys.hpp>
+
+#include <Externals/cglm/struct/mat3.h>
+#include <Externals/cglm/struct/mat4.h>
+#include <Externals/cglm/struct/cam.h>
+#include <Externals/cglm/struct/affine.h>
+
+#include <Externals/imgui/imgui.h>
 
 #include <stdlib.h> // for exit()
 
