399d49ed6e7240ef52c0104d790c818d718cc389
omd24 <omid.miresmaeili@gmail.com>
2023-02-11 17:21:33 +0100

    #ADD additional include to sample01

--- VulkanDemoFramework/Samples/01-glTFLoading/01-glTFLoading.cpp
+++ VulkanDemoFramework/Samples/01-glTFLoading/01-glTFLoading.cpp
@@ -1,3 +1,19 @@
+#include <Foundation/File.hpp>
+#include <Foundation/Gltf.hpp>
+#include <Foundation/Numerics.hpp>
+#include <Foundation/ResourceManager.hpp>
+#include <Foundation/Time.hpp>
+
 #include <Application/Window.hpp>
 
-int main() { return (0); }
+#include <stdlib.h> // for exit()
+
+int main(int argc, char** argv)
+{
+  if (argc < 2)
+  {
+    printf("No model specified, using the default model\n");
+    exit(-1);
+  }
+  return (0);
+}
